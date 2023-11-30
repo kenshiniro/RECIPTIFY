b712404c554cf190b3c012b7fc5251816e71143d
kenshiniro <kenssshinn@gmail.com>
2023-11-30 19:10:05 +0800

    part 1

---
+++ app/src/main/java/com/example/recipefinalproject/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.example.recipefinalproject;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.recipefinalproject.databinding.ActivitySplashBinding;
+
+class SplashActivity : AppCompatActivity() {
+
+private var splashScreenTime = 3000
+private var timeInterval = 100
+private var progress = 0
+private lateinit var runnable: Runnable
+private lateinit var handler: Handler
+private lateinit var binding: ActivitySplashBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.progressBar.max = splashScreenTime
+        binding.progressBar.progress = progress
+
+        handler = Handler(Looper.getMainLooper())
+        runnable = object : Runnable {
+
+
+        override fun run() {
+        if (progress < splashScreenTime) {
+        progress += timeInterval
+        binding.progressBar.progress = progress
+        handler.postDelayed(runnable, timeInterval.toLong())
+        } else {
+        Toast.makeText(this@SplashActivity, "", Toast.LENGTH_SHORT).show()
+        }
+        }
+        }
+
+        handler.postDelayed(runnable, timeInterval.toLong())
+        }
+        }
