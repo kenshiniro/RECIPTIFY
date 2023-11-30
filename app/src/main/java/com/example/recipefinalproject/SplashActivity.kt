a5a1653ecfed2ed69ff8aacd2ecaeb5a102026aa
Eksaayl <144189773+Eksaayl@users.noreply.github.com>
2023-11-30 21:28:40 +0800

    p3 and p4

--- app/src/main/java/com/example/recipefinalproject/SplashActivity.kt
+++ app/src/main/java/com/example/recipefinalproject/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.recipefinalproject;
 
+import android.content.Intent
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -9,14 +10,14 @@ import com.example.recipefinalproject.databinding.ActivitySplashBinding;
 
 class SplashActivity : AppCompatActivity() {
 
-private var splashScreenTime = 3000
-private var timeInterval = 100
-private var progress = 0
-private lateinit var runnable: Runnable
-private lateinit var handler: Handler
-private lateinit var binding: ActivitySplashBinding
+    private var splashScreenTime = 3000
+    private var timeInterval = 100
+    private var progress = 0
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
+    private lateinit var binding: ActivitySplashBinding
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -28,17 +29,18 @@ private lateinit var binding: ActivitySplashBinding
         runnable = object : Runnable {
 
 
-        override fun run() {
-        if (progress < splashScreenTime) {
-        progress += timeInterval
-        binding.progressBar.progress = progress
-        handler.postDelayed(runnable, timeInterval.toLong())
-        } else {
-        Toast.makeText(this@SplashActivity, "", Toast.LENGTH_SHORT).show()
-        }
-        }
+            override fun run() {
+                if (progress < splashScreenTime) {
+                    progress += timeInterval
+                    binding.progressBar.progress = progress
+                    handler.postDelayed(runnable, timeInterval.toLong())
+                } else {
+                    startActivities(arrayOf(Intent(this@SplashActivity, LoginActivity::class.java)));
+                    finish();
+                }
+            }
         }
 
         handler.postDelayed(runnable, timeInterval.toLong())
-        }
-        }
+    }
+}
